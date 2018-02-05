be9433537a8d03be523ee30a1a34de671b916173
Mellet Solbakk <mellet89@gmail.com>
2018-02-06 00:47:31 +0100

    :hammer: Rewrite and add auth actions that should be implemented

--- frontend/actions/authentication/AuthActionTypes.ts
+++ frontend/actions/authentication/AuthActionTypes.ts
@@ -1,18 +1,47 @@
 import AuthActionTypeKeys from './AuthActionTypeKeys'
 import { IOtherAction } from '../IOtherAction'
+import { IUserCreateResponse } from '../../api/AuthApi'
 
-export interface ISignInAction {
-  readonly type: AuthActionTypeKeys.SIGN_IN
-  readonly payload: {
-    username: string
-    password: string
-  }
+export interface ISignInPendingAction {
+  readonly type: AuthActionTypeKeys.SIGN_IN_PENDING
+}
+
+export interface ISignInFulfilledAction {
+  readonly type: AuthActionTypeKeys.SIGN_IN_FULFILLED
+  readonly payload: any
+}
+
+export interface ISignInRejectedAction {
+  readonly type: AuthActionTypeKeys.SIGN_IN_REJECTED
+  readonly payload: Error
 }
 
 export interface ISignOutAction {
   readonly type: AuthActionTypeKeys.SIGN_OUT
 }
 
-type AuthActionTypes = ISignInAction | ISignOutAction | IOtherAction
+export interface ICreateUserPendingAction {
+  readonly type: AuthActionTypeKeys.CREATE_USER_PENDING
+}
+
+export interface ICreateUserFulfilledAction {
+  readonly type: AuthActionTypeKeys.CREATE_USER_FULFILLED
+  readonly payload: IUserCreateResponse
+}
+
+export interface ICreateUserRejectedAction {
+  readonly type: AuthActionTypeKeys.CREATE_USER_REJECTED
+  readonly payload: IUserCreateResponse | Error
+}
+
+type AuthActionTypes =
+  | ISignInPendingAction
+  | ISignInFulfilledAction
+  | ISignInRejectedAction
+  | ISignOutAction
+  | ICreateUserPendingAction
+  | ICreateUserFulfilledAction
+  | ICreateUserRejectedAction
+  | IOtherAction
 
 export default AuthActionTypes
--- frontend/actions/authentication/AuthActions.ts
+++ frontend/actions/authentication/AuthActions.ts
@@ -1,14 +1,46 @@
+import { Dispatch } from 'react-redux'
+import { IStoreState } from '../../store/StoreState'
+import { IUserRegisterDetails, IUserAuthResponse } from '../../api/AuthApi'
 import AuthActionTypeKeys from './AuthActionTypeKeys'
-import { ISignOutAction, ISignInAction } from './AuthActionTypes'
+import {
+  ISignOutAction,
+  ISignInPendingAction,
+  ISignInFulfilledAction,
+  ISignInRejectedAction,
+  ICreateUserPendingAction,
+  ICreateUserFulfilledAction,
+  ICreateUserRejectedAction,
+} from './AuthActionTypes'
 
-export const signIn = (username: string, password: string): ISignInAction => ({
-  type: AuthActionTypeKeys.SIGN_IN,
-  payload: {
-    username,
-    password,
-  },
+export const signIn = (username: string, password: string) => {
+  return async (dispatch: Dispatch<IStoreState>) => {
+    // Dispatch a sign in in progress action
+    // use auth api to async authenticate
+    // Dispatch siginsuccess if ok, signinerror if failed
+  }
+}
+
+export const createUser = (userData: IUserRegisterDetails) => {
+  return async (dispatch: Dispatch<IStoreState>) => {
+    /* dispatch createUserInProgress (for loading indicator)
+     * use auth api to async create user
+     * => dispatch signInSuccess if ok
+     * => dispatch signInError if failed */
+  }
+}
+
+export const signOut = (): ISignOutAction => ({})
+
+export const signInPending = (): ISignInPendingAction => ({
+  type: AuthActionTypeKeys.SIGN_IN_PENDING,
 })
 
-export const signOut = (): ISignOutAction => ({
-  type: AuthActionTypeKeys.SIGN_OUT,
-})
+export const signInSuccess = (data: IUserAuthResponse): ISignInFulfilledAction => ({})
+
+export const signInError = (error: any): ISignInRejectedAction => ({})
+
+export const createUserPending = (): ICreateUserPendingAction => ({})
+
+export const createUserSuccess = (data: any): ICreateUserFulfilledAction => ({})
+
+export const createUserError = (): ICreateUserRejectedAction => ({})
