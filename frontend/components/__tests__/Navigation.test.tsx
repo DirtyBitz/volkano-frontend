fda45d623c9b216d9dba4433e3cf2e28e1ddd5a6
raymonshansen <raymon.s.hansen@gmail.com>
2018-02-06 14:42:37 +0100

    :hammer: Refactor Navigation tests

--- frontend/components/__tests__/Navigation.test.tsx
+++ frontend/components/__tests__/Navigation.test.tsx
@@ -23,6 +23,38 @@ describe('Layout component', () => {
     expect(homelink.length).toBe(1)
   })
 
+  describe('when signed in', () => {
+    let fakeUserData: any
+    beforeEach(() => {
+      fakeUserData = {
+        username: "testuser"
+      }
+      navigation = shallow(<Navigation userData={fakeUserData} />)
+    })
+
+    it('does not have a link to sign in page when already signed in', () => {
+      const links = navigation.find("Link")
+      
+      const signin = links.filterWhere((link: any) =>
+      link.prop("href") === "/signin"
+      )
+      
+      expect(signin.length).toBe(0)
+    })
+    
+    it('Contains link to profile page when signed in', () => {
+      const links = navigation.find("Link")
+      
+      const profile = links.filterWhere((link: any) => {
+        const linktext = link.find('a').first().text() === fakeUserData.username
+        const linkurl = link.prop("href") === "/profile"
+        return linktext && linkurl
+      })
+      
+      expect(profile.length).toBe(1)
+    })
+  })
+
   it('Has a link to sign in page when no not signed in', () => {
     const links = navigation.find("Link")
 
@@ -31,21 +63,6 @@ describe('Layout component', () => {
     )
 
     expect(signin.length).toBe(1)
-  })
-
-  it('does not have a link to sign in page when already signed in', () => {
-    const fakeUserData = {
-      username: "testuser"
-    }
-    navigation = shallow(<Navigation userData={fakeUserData} />)
-
-    const links = navigation.find("Link")
-
-    const signin = links.filterWhere((link: any) =>
-      link.prop("href") === "/signin"
-    )
-
-    expect(signin.length).toBe(0)
   })
 
   it('Should not show profile link when no userData is present', () => {
@@ -57,23 +74,4 @@ describe('Layout component', () => {
 
     expect(profile.length).toBe(0)
   })
-
-  it('Contains link to profile page when signed in', () => {
-    const fakeUserData = {
-      username: "testuser"
-    }
-    navigation = shallow(<Navigation userData={fakeUserData} />)
-
-    const links = navigation.find("Link")
-
-    const profile = links.filterWhere((link: any) => {
-      const linktext = link.find('a').first().text() === fakeUserData.username
-      const linkurl = link.prop("href") === "/profile"
-      return linktext && linkurl
-    })
-
-    expect(profile.length).toBe(1)
-
-  })
-
 })
