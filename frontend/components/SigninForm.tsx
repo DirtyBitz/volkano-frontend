64c66495266a43b5997ab8fe1a31371e495b52d7
Mellet Solbakk <mellet89@gmail.com>
2018-02-09 00:13:58 +0100

    :sparkles: Show errors on sign in form

--- frontend/components/SigninForm.tsx
+++ frontend/components/SigninForm.tsx
@@ -6,6 +6,7 @@ interface IProps {
   pristine: boolean
   reset: (event) => void
   submitting: boolean
+  errors: string[]
 }
 
 class SigninForm extends React.Component<IProps> {
@@ -19,58 +20,59 @@ class SigninForm extends React.Component<IProps> {
       <label>{label}</label>
       <div className={asyncValidating ? 'async-validating' : ''}>
         <input {...input} type={type} placeholder={label} />
-        {touched && error && <span>{error}</span>}
+        {touched &&
+          error && (
+            <span style={{ marginLeft: 10, color: 'red', fontSize: 9 }}>{error}</span>
+          )}
       </div>
     </div>
   )
 
   render() {
-    const { handleSubmit, pristine, reset, submitting } = this.props
+    const { handleSubmit, pristine, reset, submitting, errors } = this.props
 
     return (
-      <form onSubmit={handleSubmit}>
-        <Field
-          //@ts-ignore
-          name="username"
-          type="text"
-          component={this.renderField}
-          label="Username"
-        />
-
-        <Field
-          //@ts-ignore
-          name="password"
-          type="password"
-          component={this.renderField}
-          label="Password"
-        />
-
-        <div>
-          <button type="submit" disabled={submitting}>
-            Signin
-          </button>
-          <button type="button" disabled={pristine || submitting} onClick={reset}>
-            Clear Values
-          </button>
+      <div>
+        <div className="errors" style={{ color: 'red', marginBottom: 15, fontSize: 10 }}>
+          {errors && errors.map(error => <div className="error">{error}</div>)}
         </div>
-      </form>
+
+        <form onSubmit={handleSubmit}>
+          <Field
+            //@ts-ignore
+            name="username"
+            type="text"
+            component={this.renderField}
+            label="Username"
+          />
+
+          <Field
+            //@ts-ignore
+            name="password"
+            type="password"
+            component={this.renderField}
+            label="Password"
+          />
+
+          <div>
+            <button type="submit" disabled={submitting}>
+              Signin
+            </button>
+            <button type="button" disabled={pristine || submitting} onClick={reset}>
+              Clear Values
+            </button>
+          </div>
+        </form>
+      </div>
     )
   }
 }
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-
-const asyncValidate = (values /*, dispatch */) => {
-  return sleep(1000).then(() => {
-    // simulate server latency
-    if (['john', 'paul', 'george', 'ringo'].includes(values.username)) {
-      throw { username: 'That username is taken' }
-    }
-  })
-}
-
 const validate = values => {
-  const errors = {}
+  const errors = {
+    username: undefined,
+    password: undefined,
+  }
   if (!values.username) {
     errors.username = 'Required'
   }
@@ -83,6 +85,6 @@ const validate = values => {
 export default reduxForm({
   form: 'signin',
   validate,
-  //asyncValidate,
   asyncBlurFields: ['username'],
+  //@ts-ignore
 })(SigninForm)
