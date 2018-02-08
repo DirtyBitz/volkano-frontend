b53fe8e2f472a2be829a2e58bb24ff271eea04e7
Mellet Solbakk <mellet89@gmail.com>
2018-02-09 00:10:52 +0100

    :ambulance: Allow client to read access token from backend

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -27,7 +27,7 @@ module Api
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: %i[get post options]
+        resource '*', headers: :any, methods: %i[get post options], expose: ['access-token']
       end
     end
 
