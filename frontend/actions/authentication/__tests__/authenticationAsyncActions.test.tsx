ec9de3f662c8b0e67d7150225ab60f98faf87889
Mellet Solbakk <mellet89@gmail.com>
2018-02-08 20:34:23 +0100

    :umbrella: Add sign in thunk test

---
+++ frontend/actions/authentication/__tests__/authenticationAsyncActions.test.tsx
@@ -0,0 +1,25 @@
+/// <reference types="jest"/>
+import thunk from 'redux-thunk'
+import * as actions from '../AuthActions'
+import * as createMockStore from 'redux-mock-store'
+import { AuthApi } from '../../../api/AuthApi'
+
+describe('Authentication thunk actions', () => {
+  let store
+
+  beforeEach(() => {
+    //@ts-ignore
+    store = createMockStore([thunk])({})
+  })
+
+  it('Sign in action happy path', async () => {
+    const expectedActions = [
+      actions.signInPending(),
+      actions.signInSuccess(await AuthApi.authenticateUser('test', 'test')),
+    ]
+
+    await store.dispatch(actions.signIn('test', 'test'))
+
+    expect(store.getActions()).toEqual(expectedActions)
+  })
+})
