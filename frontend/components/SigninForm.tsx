b1ce3850825b7e02ab88d0126fc10ba021cb0764
Mellet Solbakk <mellet89@gmail.com>
2018-02-15 19:01:20 +0100

    :lipstick: So pretty login

--- frontend/components/SigninForm.tsx
+++ frontend/components/SigninForm.tsx
@@ -1,21 +1,75 @@
 import * as React from 'react'
 import { Field, reduxForm } from 'redux-form'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 
 const renderField = field => {
-  const { meta, type, label, asyncValidating, input } = field
+  const { meta, type, label, asyncValidating, input, iconName } = field
   return (
     <div>
-      <label>{label}</label>
-      <div className={asyncValidating ? 'async-validating' : ''}>
-        <input {...input} type={type} />
+      <div id="field" className={asyncValidating ? 'async-validating' : ''}>
+        <span className="icon">
+          <FontAwesomeIcon icon={iconName} color="#bbb" />
+        </span>
+        <input {...input} type={type} id={field.id} placeholder={label} />
         {meta.touched &&
           meta.error && (
-            <span
-              style={{ color: 'red', fontSize: 10, display: 'block', padding: '3px' }}>
-              {meta.error}
-            </span>
+            <div className="validation-error">
+              <span>{meta.error}</span>
+            </div>
           )}
       </div>
+      <style jsx>{`
+        #field {
+          position: relative;
+          display: flex;
+          align-items: center;
+          margin-bottom: 15px;
+
+          .icon {
+            background: #fff;
+            padding: 10px;
+            border-top-left-radius: 5px;
+            border-bottom-left-radius: 5px;
+            width: 40px;
+            text-align: center;
+          }
+        }
+        input {
+          padding: 10px;
+          font-size: 15px;
+          font-family: 'Montserrat', sans-serif;
+          border: 0;
+          border-top-right-radius: 5px;
+          border-bottom-right-radius: 5px;
+          border-left: 1px solid #f9f9f9;
+          outline: none;
+        }
+        .validation-error {
+          display: flex;
+          position: absolute;
+          left: calc(100% + 10px);
+          top: 0;
+          height: 100%;
+          align-items: center;
+          span {
+            background: rgba(163, 0, 0, 0.75);
+            color: #fff;
+            border-radius: 5px;
+            padding: 5px;
+            animation: fadein 0.5s;
+            white-space: nowrap;
+          }
+
+          @keyframes fadein {
+            from {
+              opacity: 0;
+            }
+            to {
+              opacity: 1;
+            }
+          }
+        }
+      `}</style>
     </div>
   )
 }
@@ -25,8 +79,22 @@ const SignInForm = props => {
   return (
     <div className="page">
       <form onSubmit={handleSubmit}>
-        <Field name="email" type="text" component={renderField} label="E-Mail" />
-        <Field name="password" type="password" component={renderField} label="Password" />
+        <Field
+          name="email"
+          type="text"
+          component={renderField}
+          label="E-Mail"
+          id="email-field"
+          iconName="user"
+        />
+        <Field
+          name="password"
+          type="password"
+          component={renderField}
+          label="Password"
+          id="pw-field"
+          iconName="lock"
+        />
         <div className="buttons">
           <button type="submit" disabled={submitting}>
             Sign In
@@ -35,35 +103,27 @@ const SignInForm = props => {
       </form>
 
       <style jsx>{`
-        @import url('https://fonts.googleapis.com/css?family=Montserrat');
         form {
           text-align: center;
           padding: 20px;
           display: table;
           margin: 0 auto;
-        }
-        button {
-          margin: 10px;
-          padding: 2px;
-          background-color: #ce1a1a;
-          color: white;
-          font-family: 'Montserrat', sans-serif;
-          width: 50px;
-          height: 25px;
+          background-color: #e9ebed;
           border-radius: 10px;
         }
-        button:hover {
-          background-color: #a02424;
+
+        button {
+          padding: 10px 20px;
+          font-size: 1em;
+          background-color: rgba(#54b45f, 0.8);
+          color: white;
+          font-family: 'Montserrat', sans-serif;
+          border-radius: 10px;
+          cursor: pointer;
+          border: 0;
         }
-        .page {
-          border: 2px solid #bcbcbc;
-          background-color: #e9ebed;
-          border-radius: 25px;
-          position: fixed;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          font-size: 20px;
+        button:hover {
+          background-color: rgba(#54b45f, 1);
         }
       `}</style>
     </div>
@@ -72,14 +132,14 @@ const SignInForm = props => {
 
 const validate = values => {
   const errors = {
-    username: undefined,
+    email: undefined,
     password: undefined,
   }
-  if (!values.username) {
-    errors.username = 'Required'
+  if (!values.email) {
+    errors.email = "E-mail can't be blank"
   }
   if (!values.password) {
-    errors.password = 'Required'
+    errors.password = "Password can't be blank"
   }
   return errors
 }
