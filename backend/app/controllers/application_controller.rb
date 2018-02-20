aa38a7ddf8ea830264ca96a5aaab29c841bf7c9e
IsakSundeSingh <isak.sunde.singh@gmail.com>
2018-02-20 15:13:09 +0100

    :ambulance: Fix bug where nickname was not permitted parameter and thus not saved

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
