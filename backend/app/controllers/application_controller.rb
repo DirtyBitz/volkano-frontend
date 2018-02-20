d7667b30aa4c325ff1e6edf41c921ed25ea81635
Mads Johansen <johansen.max@gmail.com>
2018-02-20 17:00:12 +0100

    :lock: Add authentication to ApplicationController

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  before_action :authenticate_user!, unless: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
