3d7ab745a58cd4e3eeea880d9bca63ac4278fca5
Lars Karlsen <lars.karlsen@hotmail.com>
2018-02-22 15:38:38 +0100

    :sparkles: Add api for Item

---
+++ frontend/api/ItemApi.ts
@@ -0,0 +1,39 @@
+import { Item } from '../models/Item'
+
+export interface IItemData {
+  item: Item[]
+}
+
+export class ItemApi {
+  public static getAllItems() {
+    const fakeData: IItemData = {
+      item: [
+        {
+          id: 0,
+          title: 'Cute Kitty',
+          url: 'https://i.imgur.com/gbwgfw6.jpg',
+          uid: 2,
+        },
+        {
+          id: 1,
+          title: 'Cold Kitty',
+          url: 'https://i.imgur.com/t6RdGpq.jpg',
+          uid: 2,
+        },
+        {
+          id: 2,
+          title: 'Scary Kitty',
+          url: 'https://i.imgur.com/7hN0mZ9.jpg',
+          uid: 2,
+        },
+        {
+          id: 3,
+          title: 'Evil Kitty',
+          url: 'https://i.imgur.com/uKHlfC7.jpg',
+          uid: 2,
+        },
+      ],
+    }
+    return fakeData
+  }
+}
