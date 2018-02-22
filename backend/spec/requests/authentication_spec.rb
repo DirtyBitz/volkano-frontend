18c8800d71a93fa7d1e865dda5c19d31ecd81d32
Mads Johansen <johansen.max@gmail.com>
2018-02-22 20:34:34 +0100

    :hammer: Refactor backend Auth spec

--- backend/spec/requests/authentication_spec.rb
+++ backend/spec/requests/authentication_spec.rb
@@ -3,50 +3,64 @@
 require 'rails_helper'
 
 RSpec.describe 'User authentication', type: :request do
-  let(:valid_params) do
-    {
-      email: create(:user, :confirmed, password: 'password').email,
-      password: 'password'
-    }
+  it 'can create a new user' do
+    sign_up = attributes_for(:user)
+              .merge(confirm_success_url: 'http://example.com')
+    expect { post '/auth', params: sign_up }
+      .to change { User.count }.by(1)
+    expect(response).to have_http_status(:ok)
   end
 
   context 'with valid credentials' do
-    it 'can create a new user' do
-      sign_up = attributes_for(:user)
-                .merge(confirm_success_url: 'http://dummy.com')
-
-      expect { post '/auth', params: sign_up }
-        .to change { User.count }.by(1)
-      expect(response).to have_http_status(:ok)
+    let(:valid_params) do
+      {
+        email: create(:user, :confirmed, password: 'password').email,
+        password: 'password',
+        confirm_success_url: 'http://example.com'
+      }
     end
 
-    it 'returns an auth token' do
+    it 'returns auth headers' do
       post '/auth/sign_in', params: valid_params
       expect(response).to have_http_status(:ok)
+      expect(response.headers).to include('uid')
       expect(response.headers).to include('client')
       expect(response.headers).to include('token')
     end
 
     it 'returns valid tokens' do
       post '/auth/sign_in', params: valid_params
-      params = {
+      auth_params = {
         uid: response.headers['uid'],
         token: response.headers['token'],
         client: response.headers['client']
       }
-      get '/auth/validate_token', params: params
+      get '/auth/validate_token', params: auth_params
       expect(response).to have_http_status(:ok)
     end
   end
 
-  it 'does not create a new user when password is too short' do
-    post '/auth', params: { email: 'goofus@example.com', password: 'short' }
-    expect(response).to have_http_status(:unprocessable_entity)
-  end
+  context 'with invalid params' do
+    let(:bad_params) do
+      {
+        email: 'goofus@example.com',
+        password: 'short',
+        confirm_success_url: 'http://example.com'
+      }
+    end
 
-  it 'does not grant authorization without registered account' do
-    bad_params = { email: 'goofus@example.com', password: 'password' }
-    post '/auth/sign_in', params: bad_params
-    expect(response).to have_http_status(:unauthorized)
+    it 'does not create a new user when password is too short' do
+      post '/auth', params: bad_params
+      expect(response).to have_http_status(:unprocessable_entity)
+      res = JSON.parse(response.body)
+      expect(res['errors']).to include('password')
+    end
+
+    it 'does not grant authorization without registered account' do
+      bad_params = attributes_for(:user)
+                   .merge(confirm_success_url: 'http://dummy.com')
+      post '/auth/sign_in', params: bad_params
+      expect(response).to have_http_status(:unauthorized)
+    end
   end
 end
