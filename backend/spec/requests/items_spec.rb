7d14360a45086ecb43697b39bab090f97574c4f7
Mads Johansen <johansen.max@gmail.com>
2018-02-22 23:27:39 +0100

    :hammer: Refactor Items request spec

--- backend/spec/requests/items_spec.rb
+++ backend/spec/requests/items_spec.rb
@@ -3,24 +3,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Items endpoint', type: :request do
-  let(:user) { create(:user, :confirmed, password: 'password') }
-
-  context 'with valid credentials' do
-    before(:each) do
-      post '/auth/sign_in', params: {
-        email: user.email,
-        password: 'password'
-      }
-      expect(response.content_type).to eq('application/json')
-      expect(response).to have_http_status(:ok)
-      @headers = response.headers.slice('token', 'client', 'uid')
-    end
+  context 'as a new user' do
+    let(:user) { create(:user, :confirmed) }
 
     it 'can get items' do
-      get '/items', params: {}, headers: @headers
+      get '/items', headers: user.create_new_auth_token
       expect(response).to have_http_status(:ok)
       expect(response.body).to eq('[]')
     end
+  end
+
+  context 'as an experienced user' do
+    let(:user) { create(:user_with_items) }
+
+    it 'can get items' do
+      items = user.items.to_json
+      get '/items', headers: user.create_new_auth_token
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to eq(items)
+    end
+
+    it 'can not delete others items'
+    it 'can not view others items'
   end
 
   context 'with invalid credentials' do
