4b5ebcd4c3a3cdb38e76ea1f0011cc80e28f48e5
Mads Johansen <johansen.max@gmail.com>
2018-02-22 23:27:14 +0100

    :pencil: Refactor Users factory

--- backend/spec/factories/users.rb
+++ backend/spec/factories/users.rb
@@ -1,15 +1,25 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  trait :confirmed do
+    confirmed_at { Time.now.utc }
+  end
+
   sequence :email do |n|
     "user-#{n}@example.com"
   end
 
+  sequence :password do
+    SecureRandom.alphanumeric(10)
+  end
+
   factory :user do
     email
-    password 'password'
+    password
 
     factory :user_with_items do
+      confirmed
+
       transient do
         num_items 5
       end
@@ -18,9 +28,5 @@ FactoryBot.define do
         create_list(:item, evaluator.num_items, user: user)
       end
     end
-  end
-
-  trait :confirmed do
-    confirmed_at { Time.now.utc }
   end
 end
