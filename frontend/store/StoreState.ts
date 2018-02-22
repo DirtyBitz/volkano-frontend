89735f1e592073f5bdf987b536ff811bfa0b9e98
Lars Karlsen <lars.karlsen@hotmail.com>
2018-02-22 15:23:35 +0100

    :pencil: Add item to storeState

--- frontend/store/StoreState.ts
+++ frontend/store/StoreState.ts
@@ -1,5 +1,6 @@
 import { AuthStateI } from '../reducers/authenticationReducer'
-
+import { ItemStateI } from '../reducers/itemsReducer'
 export interface IStoreState {
   readonly authentication: AuthStateI
+  readonly item: ItemStateI
 }
