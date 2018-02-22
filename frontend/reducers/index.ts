d7cf8f202852d99e346375dea9239ad9ef200494
Mellet Solbakk <mellet89@gmail.com>
2018-02-22 16:24:17 +0100

    :ambulance: Add reducer

--- frontend/reducers/index.ts
+++ frontend/reducers/index.ts
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import { reducer } from 'redux-form'
 import { IStoreState } from '../store/StoreState'
 import isAuthenticated from './authenticationReducer'
+import itemReducer from './itemReducer'
 
 const rootReducer = combineReducers<IStoreState>({
   authentication: isAuthenticated,
+  item: itemReducer,
   form: reducer,
 })
 
