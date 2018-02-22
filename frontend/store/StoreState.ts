9e93396e54d0dae8a37bffa26faf8db6df6ec25b
Lars Karlsen <lars.karlsen@hotmail.com>
2018-02-22 15:38:11 +0100

    :pencil: Change wrong pathname

--- frontend/store/StoreState.ts
+++ frontend/store/StoreState.ts
@@ -1,5 +1,5 @@
 import { AuthStateI } from '../reducers/authenticationReducer'
-import { ItemStateI } from '../reducers/itemsReducer'
+import { ItemStateI } from '../reducers/itemReducer'
 export interface IStoreState {
   readonly authentication: AuthStateI
   readonly item: ItemStateI
