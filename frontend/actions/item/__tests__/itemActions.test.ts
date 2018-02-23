4f0181a9da6a6586a410697138f240ed2641313a
Lars Karlsen <lars.karlsen@hotmail.com>
2018-02-23 16:29:36 +0100

    :umbrella: Add action test for item

---
+++ frontend/actions/item/__tests__/itemActions.test.ts
@@ -0,0 +1,34 @@
+//jest.mock('../../../api/ItemApi')
+import * as actions from '../ItemActions'
+import ItemActionTypeKeys from '../ItemActionTypeKeys'
+import { ItemApi } from '../../../api/ItemApi'
+
+describe('Item actions', () => {
+  describe('Entering "localhost:3000/collection"', () => {
+    it('loading items from backend', () => {
+      const expected = {
+        type: ItemActionTypeKeys.ITEM_PENDING,
+      }
+
+      expect(actions.itemPending()).toEqual(expected)
+    })
+    it('Creates a item success action', async () => {
+      const data = await ItemApi.getAllItems('fake-token')
+
+      const expected = {
+        type: ItemActionTypeKeys.ITEM_FULFILLED,
+        payload: data,
+      }
+
+      expect(actions.itemSuccess(data)).toEqual(expected)
+    })
+    it('Creates a item error action', () => {
+      const expected = {
+        type: ItemActionTypeKeys.ITEM_REJECTED,
+        payload: ['Error'],
+      }
+
+      expect(actions.itemError(expected.payload)).toEqual(expected)
+    })
+  })
+})
---
+++ frontend/actions/item/__tests__/itemAsyncActions.test.tsx
@@ -0,0 +1,35 @@
+//jest.mock('../../../api/ItemApi')
+import thunk from 'redux-thunk'
+import * as actions from '../ItemActions'
+import createMockStore, { MockStore } from 'redux-mock-store'
+import { ItemApi } from '../../../api/ItemApi'
+
+describe('Item thunk actions', () => {
+  let store: MockStore<{}>
+
+  beforeEach(() => {
+    store = createMockStore([thunk])({})
+  })
+
+  it('Get all items action is a thunk', () => {
+    const thunk = actions.allItems('fake-token')
+    expect(typeof thunk).toBe('function')
+  })
+
+  it('Get all items action happy path', async () => {
+    const expectedActions = [
+      actions.itemPending(),
+      actions.itemSuccess(await ItemApi.getAllItems('fake-token')),
+    ]
+
+    await store.dispatch(actions.allItems('fake-token'))
+
+    expect(store.getActions()).toEqual(expectedActions)
+  })
+
+  /*it('Get all items action sad path', async () => {
+    const expectedActions = [actions.itemPending(), actions.itemError(['Did throw'])]
+    await store.dispatch(actions.allItems('throw'))
+    expect(store.getActions()).toEqual(expectedActions)
+  })*/
+})
