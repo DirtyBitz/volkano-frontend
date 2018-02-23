824b4ce53cc473e4e1586397598e0f9e7a18a427
mariusmaeland <marius_mala92@hotmail.com>
2018-02-23 14:28:49 +0100

    :pencil: Add allow blank nickname to validations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
 
   has_many :items, dependent: :destroy
-  validates :nickname, uniqueness: true
+  validates :nickname, uniqueness: true, allow_blank: true
 end
