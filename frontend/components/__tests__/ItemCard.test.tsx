ed3079bad0f87a2c285ef222f6e3eaad28b489c4
IsakSundeSingh <isak.sunde.singh@gmail.com>
2018-02-26 16:58:07 +0100

    :umbrella: Add tests for ItemCard component

---
+++ frontend/components/__tests__/ItemCard.test.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import { shallow, ShallowWrapper } from 'enzyme'
+import ItemCard from '../ItemCard'
+import { Item } from '../../models/Item'
+
+describe('ItemCard component', () => {
+  const item = {
+    id: 1,
+    title: 'one examply boy',
+    url: 'http://example.com/example.png',
+    uid: 2,
+    tags: ['example', 'boy'],
+    categories: ['png', 'image/png'],
+  }
+
+  it('should show tags', () => {
+    const itemCard = shallow(<ItemCard item={item} />)
+    const tags = itemCard.find('.tag')
+    expect(tags.length).toBe(2)
+  })
+
+  it('should show title', () => {
+    const itemCard = shallow(<ItemCard item={item} />)
+    const title = item.title
+    expect(itemCard.contains(title)).toBe(true)
+  })
+
+  it('should contain a conditional image based on image type ')
+})
