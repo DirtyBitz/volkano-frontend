076ae74796fdb1776ee28111440044d382d796d8
Mellet Solbakk <mellet89@gmail.com>
2018-02-26 19:29:54 +0100

    :sparkles: :lipstick: Allow svg and youtube clip as item

--- frontend/components/ItemCard.tsx
+++ frontend/components/ItemCard.tsx
@@ -1,39 +1,88 @@
 import * as React from 'react'
 import { Item } from '../models/Item'
+import YouTube from 'react-youtube'
 
 interface Props {
   item: Item
 }
+
 export default class ItemCard extends React.Component<Props> {
   render() {
-    const { url, title, tags, id } = this.props.item
+    const { item } = this.props
     return (
-      <div key={id} id="picture">
-        <figure>
-          <img src={url} width="250" height="175" />
-          <figcaption>{title}</figcaption>
-        </figure>
-        <ul>
-          {tags &&
-            tags.map((tag: string) => (
-              <span key={tag} className="tag">
-                {tag}
-              </span>
-            ))}
-        </ul>
-
+      <div className="item-wrap">
+        <div className="item">
+          {this.renderItem()}
+          {item.tags.map((tag: string) => (
+            <span className="tag" key={tag}>
+              {tag}
+            </span>
+          ))}
+        </div>
         <style jsx>{`
-          #picture {
-            text-align: center;
-            display: inline-block;
-            margin: 5px;
-            border: 2px solid black;
-            border-radius: 15px;
-            background-color: #e9ebed;
+          .item-wrap {
+            padding-bottom: 15px;
+            padding-right: 15px;
+            width: 20%;
           }
-          img {
-            border-top-left-radius: 12px;
-            border-top-right-radius: 12px;
+
+          .item {
+            text-align: center;
+            border-radius: 5px;
+            overflow: hidden;
+          }
+        `}</style>
+      </div>
+    )
+  }
+
+  private renderItem = () => {
+    const { title } = this.props.item
+    return (
+      <figure>
+        <div className="image-container">{this.renderFileType()}</div>
+        <figcaption>{title}</figcaption>
+        <style jsx>{`
+          .image-container {
+            height: 175px;
+            display: flex;
+            align-items: center;
+            overflow: hidden;
+          }
+
+          figcaption {
+            background: #ce1a1a;
+            color: #fff;
+            padding: 5px 10px;
+          }
+        `}</style>
+      </figure>
+    )
+  }
+
+  private renderFileType = () => {
+    const { url } = this.props.item
+    if (url.includes('youtube')) return this.renderYouTube()
+    else return this.renderImage()
+  }
+
+  private renderYouTube = () => {
+    const { url } = this.props.item
+
+    const videoId = url.split('v=')[1]
+    const opts = {
+      width: '100%',
+      playerVars: {
+        autoplay: 0,
+      },
+    }
+
+    return (
+      <div>
+        <YouTube videoId={videoId} opts={opts} />
+        <style jsx>{`
+          div {
+            width: 100%;
           }
           .tag {
             background-color: gray;
@@ -41,5 +90,10 @@ export default class ItemCard extends React.Component<Props> {
         `}</style>
       </div>
     )
+  }
+
+  private renderImage() {
+    const { url } = this.props.item
+    return <img src={url} width="100%" />
   }
 }
