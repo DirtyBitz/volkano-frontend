38d43fbc183a8a072c7ec4bb0c800ba2ab50cc29
IsakSundeSingh <isak.sunde.singh@gmail.com>
2018-02-27 14:52:29 +0100

    :elephant: Add seed data so page looks good

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,4 +7,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(email: 'test@example.com', password: 'password', confirmed_at: Time.now)
+u = User.create(email: 'test@example.com', password: 'password', confirmed_at: Time.now)
+
+u.items.create(title: "Lanky doggo", url: "https://exceptionalanimals.files.wordpress.com/2011/03/langy_lycaon.jpg", tag_list: "doggo, lanky").save
+u.items.create(title: "Browno Doggo", url: "https://i.redd.it/p58vnipwhk7z.jpg", tag_list: "doggo, cholocoto").save
