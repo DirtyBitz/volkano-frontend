f506a26894ca3eda6df754b3a5d6d2dfc4af7607
IsakSundeSingh <isak.sunde.singh@gmail.com>
2018-02-27 15:05:30 +0100

    :pencil: Add timestamps to serializer

--- backend/app/serializers/item_serializer.rb
+++ backend/app/serializers/item_serializer.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :title, :url, :tag_list, :category_list, :created_at, :updated_at
+  attributes :id, :title, :url, :tag_list,
+             :category_list, :created_at, :updated_at
 end
