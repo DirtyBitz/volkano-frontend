8c5d5d6fddf07c3d32048e172a3826c4c9cc37fc
Mads Johansen <johansen.max@gmail.com>
2018-02-27 17:06:51 +0100

    :sparkles: Add VolkanoRequest to wrap axios

---
+++ frontend/api/VolkanoRequest.ts
@@ -0,0 +1,5 @@
+export class VolkanoRequest {
+  public static noop() {
+    return
+  }
+}
---
+++ frontend/api/__tests__/VolkanoRequest.test.ts
@@ -0,0 +1,11 @@
+import { VolkanoRequest } from '../VolkanoRequest'
+
+describe('Volkano request adapter', () => {
+  it('should update token on successful request')
+  it('should not change token on server error response')
+  it('should sign out user on unauthorized response')
+  it('calls axios.post on post requests')
+  it('calls axios.get on get requests')
+  it('calls axios.put on put requests')
+  it('calls axios.patch on patch requests')
+})
