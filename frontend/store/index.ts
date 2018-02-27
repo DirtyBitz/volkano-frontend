87d0d98fba8422a65cca84c885fd6d98631290b2
Mellet Solbakk <mellet89@gmail.com>
2018-02-27 14:38:01 +0100

    :ambulance: :racehorse: Redux persist tweaks to act as expected

--- frontend/store/index.ts
+++ frontend/store/index.ts
@@ -1,50 +1,21 @@
-import { createStore, applyMiddleware } from 'redux'
-import { persistStore, persistReducer } from 'redux-persist'
-import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import storage from 'redux-persist/lib/storage'
-import reducers from '../reducers'
 import { IStoreState } from './StoreState'
 import { authInitialState } from '../reducers/authenticationReducer'
 import { collectionInitialState } from '../reducers/collectionReducer'
 
-export const initialState: IStoreState = {
+import createStoreFromServer from './ServerStore'
+import createStoreFromClient from './ClientStore'
+
+export const _initialState: IStoreState = {
   authentication: authInitialState,
   collection: collectionInitialState,
 }
 
-/**
- * Client state
- */
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['form'],
-}
-
-const persistedReducer = persistReducer(persistConfig, reducers)
-
-const clientStore = createStore(
-  persistedReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(thunkMiddleware))
-)
-
-persistStore(clientStore)
-
-/**
- * Server state
- */
-const serverStore = createStore(
-  reducers,
-  initialState,
-  composeWithDevTools(applyMiddleware(thunkMiddleware))
-)
-
-export const store = (initialState, props) => {
+export default (initialState = _initialState, props) => {
   if (props.isServer) {
-    return serverStore
+    console.log('Server store')
+    return createStoreFromServer(initialState, props)
   } else {
-    return clientStore
+    console.log('Client store')
+    return createStoreFromClient(initialState)
   }
 }
