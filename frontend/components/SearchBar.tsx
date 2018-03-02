d2abac39f36150a850705cb6c69a2d910ec02233
mariusmaeland <marius_mala92@hotmail.com>
2018-03-02 10:34:53 +0100

    :lipstick: Add sexy style to the tag search bar

--- frontend/components/SearchBar.tsx
+++ frontend/components/SearchBar.tsx
@@ -16,7 +16,7 @@ interface IProps {
 export class SearchBar extends React.Component<IProps, IState> {
   private search = (input: string) => {
     return new Promise(resolve => {
-      resolve([{ label: 'test', value: 'test' }])
+      resolve([{ label: 'test', value: 'test' }, { label: 'test2', value: 'test2' }])
     })
   }
 
@@ -28,16 +28,73 @@ export class SearchBar extends React.Component<IProps, IState> {
     this.props.addTag(newTag)
   }
 
+  private renderTag = (tag: any, remove: any) => {
+    const button = <button onClick={remove}>X</button>
+
+    return (
+      <li key={tag.value}>
+        {tag.label}
+        {button}
+      </li>
+    )
+  }
+
   render() {
     const { addTag, clearTags, removeTag, tags } = this.props
     return (
-      <TagBoxAsync
-        selected={tags}
-        onSelect={this.handleAddTag}
-        removeTag={removeTag}
-        fetch={this.search}
-        backspaceDelete={true}
-      />
+      <div>
+        <TagBoxAsync
+          selected={tags}
+          onSelect={this.handleAddTag}
+          removeTag={removeTag}
+          fetch={this.search}
+          backspaceDelete={true}
+          renderTag={this.renderTag}
+        />
+        <style global jsx>{`
+          .tag-box {
+            border: 1px solid #bbb;
+            display: flex;
+            padding: 5px;
+            border-radius: 3px;
+            position: relative;
+
+            input {
+              flex: 1;
+              outline: none;
+              padding: 4px 5px;
+              border: none;
+              font-size: 1em;
+            }
+
+            .autocomplete {
+              position: absolute;
+              left: 0;
+              top: 100%;
+              border: 1px solid #bbb;
+              background: white;
+              list-style: none;
+
+              li.considering {
+                background: pink;
+              }
+            }
+          }
+          .tag-box-pills {
+            list-style: none;
+            display: flex;
+
+            li {
+              margin-right: 5px;
+              background: #e2e2e2;
+              padding: 3px 5px;
+              font-size: 0.85em;
+              border-radius: 3px;
+              border: 1px solid #bababa;
+            }
+          }
+        `}</style>
+      </div>
     )
   }
 }
