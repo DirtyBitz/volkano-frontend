b06c375d79ebbfeb06b6893747ea73ef92a513e3
Mads Johansen <johansen.max@gmail.com>
2018-03-26 23:21:24 +0200

    :pencil: Only show link to MailCatcher in development

--- frontend/pages/emailconfirmation.tsx
+++ frontend/pages/emailconfirmation.tsx
@@ -7,8 +7,9 @@ const redirect = props => {
   return (
     <Layout title="Ya gon get emails now">
       <h1>Account created</h1>
-      <p>An account has been created and a confirmation email has been sent somewhere.</p>
-      <a href="http://localhost:1080">Well well, confirm account here.</a>
+      <p>An account has been created and a confirmation email has been sent your email address.</p>
+      { process.env.NODE_ENV !== 'production' &&
+        <a href="http://localhost:1080">In development you can confirm your account here.</a>}
     </Layout>
   )
 }
