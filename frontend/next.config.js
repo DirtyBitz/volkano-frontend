0dbe34ed5ec761a0cbd0a5000ba7ff4597eb2575
Mads Johansen <johansen.max@gmail.com>
2018-03-28 23:29:06 +0200

    :rocket: Make environment dependent on Heroku variable

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,39 +1,31 @@
 const withTypescript = require('@zeit/next-typescript')
-const { PHASE_PRODUCTION_SERVER, PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
 module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return withTypescript({
-      webpack(config, options) {
-        return config
-      },
-      publicRuntimeConfig: {
-        ENV: 'development',
-        BACKEND_URL: 'http://localhost:5000',
-        FRONTEND_URL: 'http://localhost:3000',
-      },
-    })
-  } else if (phase === PHASE_PRODUCTION_SERVER) {
-    return withTypescript({
-      webpack(config, options) {
-        return config
-      },
-      publicRuntimeConfig: {
-        ENV: 'production',
-        BACKEND_URL: 'https://api.volka.no',
-        FRONTEND_URL: 'https://volka.no',
-      },
-    })
+  const current_env = process.env.HEROKU_ENV
+  var publicRuntimeConfig = {
+    ENV: 'development',
+    BACKEND_URL: 'http://localhost:5000',
+    FRONTEND_URL: 'http://localhost:3000',
   }
-  /* Staging */
+
+  if (current_env === 'production') {
+    publicRuntimeConfig = {
+      ENV: current_env,
+      BACKEND_URL: 'https://api.volka.no',
+      FRONTEND_URL: 'https://volka.no',
+    }
+  } else if (current_env === 'staging') {
+    publicRuntimeConfig = {
+      ENV: current_env,
+      BACKEND_URL: 'https://beta-api.volka.no',
+      FRONTEND_URL: 'https://beta.volka.no',
+    }
+  }
+
   return withTypescript({
     webpack(config, options) {
       return config
     },
-    publicRuntimeConfig: {
-      ENV: 'staging',
-      BACKEND_URL: 'https://beta.api.volka.no',
-      FRONTEND_URL: 'https://beta.volka.no',
-    },
+    publicRuntimeConfig,
   })
 }
