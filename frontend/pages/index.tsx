3ffb829888ab21408516734af6db294b6743192f
IsakSundeSingh <isak.sunde.singh@gmail.com>
2018-04-03 15:10:19 +0200

    :sparkles: Add beautiful page descriptions for future customers <3

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -26,42 +26,21 @@ export class App extends React.Component<{}, {}> {
 
         <div id="info-boxes">
           <div className="info-box">
-            <h3>Feature 1</h3>
+            <h3>Our vision</h3>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the
-              1500s, when an unknown printer took a galley of type and scrambled it to
-              make a type specimen book. It has survived not only five centuries, but also
-              the leap into electronic typesetting, remaining essentially unchanged. It
-              was popularised in the 1960s with the release of Letraset sheets containing
-              Lorem Ipsum passages, and more recently with desktop publishing software
-              like Aldus PageMaker including versions of Lorem Ipsum.
+              Our goal through this project is to bring you joy when seeing images you would love to collect online. We have a wish that we can end the frustration people experience when they see an image they want to collect, but do not want to download it onto their current device.
             </p>
           </div>
           <div className="info-box">
-            <h3>Feature 2</h3>
+            <h3>Our product</h3>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the
-              1500s, when an unknown printer took a galley of type and scrambled it to
-              make a type specimen book. It has survived not only five centuries, but also
-              the leap into electronic typesetting, remaining essentially unchanged. It
-              was popularised in the 1960s with the release of Letraset sheets containing
-              Lorem Ipsum passages, and more recently with desktop publishing software
-              like Aldus PageMaker including versions of Lorem Ipsum.
+              Volkano is a place where you can easily save images, gifs, youtube-videos and more you come across online, in a single place. You can also browse your collection and easily share your items.
             </p>
           </div>
           <div className="info-box">
-            <h3>Feature 3</h3>
+            <h3>Who are we?</h3>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the
-              1500s, when an unknown printer took a galley of type and scrambled it to
-              make a type specimen book. It has survived not only five centuries, but also
-              the leap into electronic typesetting, remaining essentially unchanged. It
-              was popularised in the 1960s with the release of Letraset sheets containing
-              Lorem Ipsum passages, and more recently with desktop publishing software
-              like Aldus PageMaker including versions of Lorem Ipsum.
+              We are a group of students from UiT The Arctic University of Norway working on a group project to create something beautiful.
             </p>
           </div>
         </div>
@@ -105,6 +84,12 @@ export class App extends React.Component<{}, {}> {
               border-radius: 5px;
               border: 1px solid #eee;
               background: #fff;
+              text-align: center;
+              max-width: 33%;
+
+              h3 {
+                padding-bottom: 10px;
+              }
             }
           }
         `}</style>
