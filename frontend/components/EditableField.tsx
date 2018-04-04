8ba2bcc2446e276494ed41fdb4caa0ef51cd02eb
raymonshansen <raymon.s.hansen@gmail.com>
2018-04-04 11:28:35 +0200

    :lipstick: Add profile page fields are shorter and more sexy now.

--- frontend/components/EditableField.tsx
+++ frontend/components/EditableField.tsx
@@ -90,6 +90,7 @@ class EditableField extends React.Component<IProps, IState> {
         <style jsx>{`
           .editable-field {
             border: 1px solid #bbb;
+            width: 40%;
             display: flex;
             background: #f9f9f9;
             border-radius: 5px;
@@ -111,7 +112,7 @@ class EditableField extends React.Component<IProps, IState> {
               padding: 5px 10px 5px 10px;
               white-space: nowrap;
               border-right: 1px solid #bbb;
-              min-width: 100px;
+              min-width: 150px;
             }
 
             .value,
