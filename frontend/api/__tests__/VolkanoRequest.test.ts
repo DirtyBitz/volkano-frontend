5e24c5cd89fa5167b8010aa7ce85abeb064f8a75
Mads Johansen <johansen.max@gmail.com>
2018-04-05 13:22:50 +0200

    :umbrella: Add test for updating session on 422 response

--- frontend/api/__tests__/VolkanoRequest.test.ts
+++ frontend/api/__tests__/VolkanoRequest.test.ts
@@ -33,11 +33,48 @@ describe('Volkano request adapter', () => {
     expect(token).toBe(session.token)
   })
 
+  it('should update user on successful request', async () => {
+    const newToken = 'new-token'
+    const expectedUser = {
+      email: 'test@example.com',
+      id: 0,
+      name: null,
+      nickname: null,
+    }
+    const userJson: IUserJson = {
+      ...expectedUser,
+      uid: 'test@example.com',
+      image: null,
+      provider: null,
+    }
+
+    mock.onPost().reply(200, { data: userJson }, { token: newToken })
+
+    await VolkanoRequest.post('/auth/sign_in', {})
+    const { user, token } = getSession()
+    expect(user).toEqual(expectedUser)
+    expect(token).toEqual(newToken)
+  })
+
   it('should update token on successful request', async () => {
     const newToken = 'new-token'
     mock.onGet().reply(200, { data: 'hello' }, { token: newToken })
 
     await VolkanoRequest.get('/')
+
+    expect(getSession().token).toEqual(newToken)
+  })
+
+  it('should update token on unprocessable entity', async () => {
+    const newToken = 'new-token'
+    mock.onPost().reply(422, { data: { errors: ['invalid item'] } }, { token: newToken })
+
+    try {
+      await VolkanoRequest.post('/', { item: 'yep' })
+      expect('this should never happen').toBe(true)
+    } catch (error) {
+      expect(error.status).toBe(422)
+    }
 
     expect(getSession().token).toEqual(newToken)
   })
@@ -65,45 +102,22 @@ describe('Volkano request adapter', () => {
     expect(getSession().token).toEqual(session.token)
   })
 
-  it('should send PUT requests with provided parameters', async () => {
-    mock.onPut().reply(config => {
-      const params = config.params
-      return [200, params, { token: undefined }]
-    })
+  it('should send PUT requests with provided data', async () => {
+    mock
+      .onPut()
+      .reply(config => [200, { data: JSON.parse(config.data) }, { token: undefined }])
 
     const response = await VolkanoRequest.put('/', { nickname: 'joe' })
-
     expect(response.data.nickname).toEqual('joe')
   })
 
-  it('should send DELETE requests with provided parameters', async () => {
-    mock.onDelete().reply(config => {
-      const params = config.params
-      return [204, params, { token: undefined }]
-    })
+  it('should send DELETE requests with provided data', async () => {
+    mock
+      .onDelete()
+      .reply(config => [204, { data: JSON.parse(config.data) }, { token: undefined }])
 
     const response = await VolkanoRequest.delete('/', { id: 1 })
 
     expect(response.data.id).toEqual(1)
-  })
-
-  it('should update user on successful request', async () => {
-    const newToken = 'new-token'
-    const userJson: IUserJson = {
-      id: 0,
-      uid: 'test@example.com',
-      email: 'test@example.com',
-      nickname: null,
-      name: null,
-      image: null,
-      provider: null,
-    }
-    const user: IUser = { email: 'test@example.com', id: 0, name: null, nickname: null }
-
-    mock.onPost().reply(200, { data: userJson }, { token: newToken })
-
-    await VolkanoRequest.post('/auth/sign_in', {})
-
-    expect(getSession().user).toEqual(user)
   })
 })
