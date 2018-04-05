7972628f4bbf31f6ca9c013c7576d73c28a1e0f4
larskarlsen <lars.karlsen@hotmail.com>
2018-04-05 15:47:06 +0200

    :ambulance: Fix bug with cookies for auth pages

--- frontend/utils/withAuth.tsx
+++ frontend/utils/withAuth.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import Router from 'next/router'
 import { getSession } from './Session'
+import { BeatLoader } from 'react-spinners'
+import { Layout } from '../components/Layout'
 
 interface IProps {
   isSignedIn: boolean
@@ -15,7 +17,11 @@ export function withAuth(WrappedComponent) {
     static async getInitialProps({ req }) {
       if (req) {
         const cookies = req.headers.cookie
-        return { isSignedIn: !!cookies }
+        return {
+          isSignedIn: cookies
+            ? cookies.split(';').filter(cookie => cookie.includes('session')).length == 1
+            : false,
+        }
       } else {
         const session = getSession()
         return { isSignedIn: !!session }
@@ -28,7 +34,20 @@ export function withAuth(WrappedComponent) {
     }
 
     render() {
-      if (!this.props.isSignedIn) return null
+      if (!this.props.isSignedIn)
+        return (
+          <Layout>
+            <div
+              style={{
+                'text-align': 'center',
+                position: 'relative',
+                top: '50%',
+                transform: 'translateY(-50%)',
+              }}>
+              <BeatLoader color="#444" size={20} />
+            </div>
+          </Layout>
+        )
       return <WrappedComponent {...this.props} />
     }
   }
