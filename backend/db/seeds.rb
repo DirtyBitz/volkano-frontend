9a4491d2d3f9157aabc9fb893774ebdc10f462cd
mariusmaeland <marius_mala92@hotmail.com>
2018-04-17 14:30:53 +0200

    :umbrella: :elephant: Add new test user for Cypress specs

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,6 +5,11 @@ u.reload
 u.tokens = nil
 u.save
 
+u2 = User.create(email: 'test@test.com', password: 'password', confirmed_at: Time.now.utc)
+u2.reload
+u2.tokens = nil
+u2.save
+
 URLS = ['https://i.imgur.com/Hb6S4V5.jpg',
   'https://i.imgur.com/1dOwTta.jpg',
   'https://i.imgur.com/tCjIAXI.jpg',
