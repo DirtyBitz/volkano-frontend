803000a223ad16aff47661092446483a1d2d3977
IsakSundeSingh <isak.sunde.singh@gmail.com>
2018-04-23 16:29:03 +0200

    :umbrella: :arrow_down: Add tests for videos and remove mimetype dependency

--- backend/app/lib/collector.rb
+++ backend/app/lib/collector.rb
@@ -1,18 +1,39 @@
 # frozen_string_literal: true
 
-require 'mime/types'
-
 class Collector
   def initialize(url)
     @url = url
   end
 
   def collect
-    host = host_without_www
-    filetype = File.extname(@url).tr('.', '')
-    mimetype = MIME::Types.type_for(filetype).first
+    { type: type, categories: categories }
+  end
 
-    { categories: [host, filetype, mimetype] }
+  def categories
+    [host_without_www, filetype, type].uniq
+  end
+
+  def filetype
+    if @url.match?(%r{\A(https?\://)?(www\.)?(youtube\.com|youtu\.?be)/.+\z})
+      return 'youtube'
+    end
+
+    File.extname(@url).tr('.', '')
+  end
+
+  def type
+    case filetype
+    when /youtube/
+      'youtube'
+    when /jpe?g|gif|png|svg/
+      'image'
+    when /mp4|mov|avi|mkv/
+      'video'
+    when /mp3|ogg|wav|flac|aac|wma/
+      'audio'
+    else
+      raise InvalidType
+    end
   end
 
   def host_without_www
@@ -20,5 +41,8 @@ class Collector
     uri = URI.parse("http://#{@url}") if uri.scheme.nil?
     host = uri.host.downcase
     host.start_with?('www.') ? host[4..-1] : host
+  end
+
+  class InvalidType < StandardError
   end
 end
--- backend/spec/lib/collector_spec.rb
+++ backend/spec/lib/collector_spec.rb
@@ -7,8 +7,41 @@ RSpec.describe Collector do
   describe 'given valid url' do
     it 'should collect an image' do
       item = Collector.new('https://example.com/img.jpg').collect
+
       expect(item).to include(
-        categories: ['example.com', 'jpg', 'image/jpeg']
+        categories: include('example.com', 'jpg', 'image'),
+        type: 'image'
+      )
+    end
+
+    it 'should collect a video' do
+      item = Collector.new('https://example.com/vid.mp4').collect
+
+      expect(item).to include(
+        categories: include('example.com', 'mp4', 'video'),
+        type: 'video'
+      )
+    end
+
+    it 'should collect a youtube-video' do
+      item = Collector.new(
+        'https://www.youtube.com/watch?v=D-UmfqFjpl0'
+      ).collect
+
+      expect(item).to include(
+        categories: include('youtube.com', 'youtube'),
+        type: 'youtube'
+      )
+    end
+
+    it 'should collect a shortened youtube-link' do
+      item = Collector.new(
+        'https://youtu.be/D-UmfqFjpl0'
+      ).collect
+
+      expect(item).to include(
+        categories: ['youtu.be', 'youtube'],
+        type: 'youtube'
       )
     end
   end
