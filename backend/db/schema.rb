775df35220d64c47d96163eddc3cedf61135816c
IsakSundeSingh <isak.sunde.singh@gmail.com>
2018-04-25 17:56:25 +0200

    :elephant: Add size attribute to items

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424080557) do
+ActiveRecord::Schema.define(version: 20180425155130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180424080557) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "mediatype"
+    t.integer "size"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
