a328e074f9f622ad1957fc921c211ef727e477b0
IsakSundeSingh <isak.sunde.singh@gmail.com>
2018-04-26 10:19:02 +0200

    :elephant: :fire: Merge size and mediatype migrations

--- backend/db/migrate/20180424080557_add_mediatype_and_size_to_items_and_populate_categories.rb
+++ backend/db/migrate/20180424080557_add_mediatype_and_size_to_items_and_populate_categories.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
-class AddMediatypeToItemsAndPopulateCategories < ActiveRecord::Migration[5.1]
+class AddMediatypeAndSizeToItemsAndPopulateCategories < ActiveRecord::Migration[5.1]
   def up
     add_column :items, :mediatype, :string
+    add_column :items, :size, :integer
     Item.all.each do |item|
       collected = Collector.new(item.url).collect
       item.mediatype = collected.fetch(:mediatype, 'invalid')
       item.category_list = collected[:categories]
+      item.size = collected[:size]
       item.save!
     end
   end
 
   def down
     remove_column :items, :mediatype
+    remove_column :items, :size
     Item.all.each do |item|
       item.category_list = nil
       item.save!
