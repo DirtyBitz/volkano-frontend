940c596c9ab3a1efa5b221e4a0a4299d31676331
IsakSundeSingh <isak.sunde.singh@gmail.com>
2018-05-08 12:09:55 +0200

    :pencil: Bookmarklet now loads code from our site
    To ensure that we could use the bookmarklet during development, a dirty hack is used. A global variable is defined by the bookmarklet, which the code from our servers use to add the item to. This ensures that we can use the bookmarklet during development, by changing the links in the bookmarklet to point towards our beta-site, as that is what we use.

--- frontend/bookmarklet/bookmarklet.js
+++ frontend/bookmarklet/bookmarklet.js
@@ -1,71 +1,19 @@
 javascript: (function () {
   startup();
   function startup() {
-    let initialized = document.getElementById("VolkaScript");
+    const baseURL = 'https://volka.no';
+    let initialized = document.getElementById('VolkaScript');
     if (initialized) {
-      alert("Please click an image to add it, or right-click to abort.");
+      alert('Please click an image to add it, or right-click to abort.');
       return;
     }
 
-    initialized = document.createElement("div");
-    initialized.setAttribute("id", "VolkaScript");
+    initialized = document.createElement('script');
+    initialized.setAttribute('src', `${baseURL}/static/bookmarklet.js`);
+    initialized.setAttribute('id', 'VolkaScript');
 
-    let head = document.getElementsByTagName("head")[0];
+    let head = document.getElementsByTagName('head')[0];
     head.appendChild(initialized);
-
-    let style = document.createElement("style");
-    style.setAttribute("id", "VolkaStyle");
-    style.innerHTML = `
-    img {
-      transition: transform .5s;
-    }
-
-    img:hover {
-      border: 2px solid #8db517 !important;
-      cursor: crosshair !important;
-      transform: scale(1.05);
-    }`;
-    head.appendChild(style);
-
-    let images = document.getElementsByTagName("img");
-    for (let img of images) {
-      img.addEventListener("click", clickfunc);
-    }
-
-    document.addEventListener("contextmenu", abort, false);
-  }
-
-  function abort(e) {
-    e.preventDefault();
-    cleanup();
-    return false;
-  }
-
-  function clickfunc(e) {
-    e.preventDefault();
-    cleanup();
-    const img = e.target;
-    let url = `https://volka.no/additem?url=${img.src}`;
-
-    if (img.title) {
-      url += `&title=${img.title}`
-    } else if (img.alt) {
-      url += `&title=${img.alt}`
-    }
-
-    window.open(url)
-  };
-
-  function cleanup() {
-    let images = document.getElementsByTagName("img");
-    for (let img of images) {
-      img.removeEventListener("click", clickfunc);
-    }
-
-    let style = document.getElementById("VolkaStyle");
-    style.parentNode.removeChild(style);
-
-    let volka = document.getElementById("VolkaScript");
-    volka.parentNode.removeChild(volka);
   }
 })();
+var volkanoURLPleaseNoConflictingVariableNames = 'https://volka.no';
---
+++ frontend/static/bookmarklet.js
@@ -0,0 +1,101 @@
+(function() {
+  startup();
+  const volkaURL = volkanoURLPleaseNoConflictingVariableNames;
+  console.log(volkanoURLPleaseNoConflictingVariableNames, volkaURL);
+  function startup() {
+    let style = document.createElement('style');
+    style.setAttribute('id', 'VolkaStyle');
+    style.innerHTML = `
+    img {
+      transition: transform .5s;
+    }
+
+    img:hover {
+      border: 2px solid #8db517 !important;
+      cursor: crosshair !important;
+      transform: scale(1.05);
+    }`;
+    const head = document.getElementsByTagName('head')[0];
+    head.appendChild(style);
+
+    const images = document.getElementsByTagName('img');
+    for (const img of images) {
+      img.addEventListener('click', mediaClick);
+    }
+
+    const videos = document.getElementsByTagName('video');
+    for (const video of videos) {
+      video.addEventListener('click', videoClick);
+    }
+
+    const sounds = document.getElementsByTagName('video');
+    for (const sound of sounds) {
+      sound.addEventListener('click', mediaClick);
+    }
+
+    document.addEventListener('contextmenu', abort, false);
+  }
+
+  function abort(e) {
+    e.preventDefault();
+    cleanup();
+    return false;
+  }
+
+  function mediaClick(e) {
+    e.preventDefault();
+    cleanup();
+    const img = e.target;
+    let url = `${volkaURL}/additem?url=${img.src}`;
+
+    if (img.title) {
+      url += `&title=${img.title}`;
+    } else if (img.alt) {
+      url += `&title=${img.alt}`;
+    }
+
+    window.open(url);
+  }
+
+  function videoClick(e) {
+    e.preventDefault();
+    cleanup();
+    const video = e.target;
+    let src;
+    if (video.src === '') {
+      src = video.getElementsByTagName('source')[0].src;
+      if (src === '' || typeof src === 'undefined') {
+        alert('Could not find source of element, try adding it by right-clicking the element and copying the link.');
+        return;
+      }
+    } else {
+      src = video.src;
+    }
+
+    const url = `${volkaURL}/additem?url=${src}`;
+    window.open(url);
+  }
+
+  function cleanup() {
+    let images = document.getElementsByTagName('img');
+    for (let img of images) {
+      img.removeEventListener('click', mediaClick);
+    }
+
+    const videos = document.getElementsByTagName('video');
+    for (const video of videos) {
+      video.removeEventListener('click', videoClick);
+    }
+
+    const sounds = document.getElementsByTagName('video');
+    for (const sound of sounds) {
+      sound.removeEventListener('click', mediaClick);
+    }
+
+    let style = document.getElementById('VolkaStyle');
+    style && style.parentNode.removeChild(style);
+
+    let volka = document.getElementById('VolkaScript');
+    volka && volka.parentNode.removeChild(volka);
+  }
+})();
