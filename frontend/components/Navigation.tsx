30d60e77da4a9addbadbe9c6c55933b9732fbec7
Mads Johansen <johansen.max@gmail.com>
2018-05-09 22:06:26 +0200

    :fire: Remove unused imports

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import Link from 'next/link'
-import Router from 'next/router'
 import { IUser } from '../models/User'
 import { VolkaButton } from './VolkaButton'
 import {
