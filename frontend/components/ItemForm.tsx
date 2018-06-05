1fcd05d3c5b0e1ae15b247b3f86bf03d70080889
Mads Johansen <johansen.max@gmail.com>
2018-06-05 20:39:54 +0200

    :hammer: :sparkles: Remove redundant renderField and refactor CreateItemForm

--- frontend/components/ItemForm.tsx
+++ frontend/components/ItemForm.tsx
@@ -1,89 +1,10 @@
 import * as React from 'react'
 import { Field, reduxForm } from 'redux-form'
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
-import { faCopy, faEdit, faTags } from '@fortawesome/fontawesome-free-solid'
 import { Button } from 'semantic-ui-react'
-
-const renderField = field => {
-  const { meta, type, label, asyncValidating, input, iconName, inputValue } = field
-  return (
-    <div>
-      <div id="field" className={asyncValidating ? 'async-validating' : ''}>
-        <span className="icon">
-          <FontAwesomeIcon icon={iconName} color="#bbb" />
-        </span>
-        <input
-          {...input}
-          type={type}
-          id={field.id}
-          value={inputValue}
-          placeholder={label}
-        />
-        {meta.touched &&
-          meta.error && (
-            <div className="validation-error">
-              <span>{meta.error}</span>
-            </div>
-          )}
-      </div>
-      <style jsx>{`
-        #field {
-          position: relative;
-          display: flex;
-          align-items: center;
-          margin-bottom: 15px;
-
-          .icon {
-            background: #fff;
-            padding: 10px;
-            border-top-left-radius: 5px;
-            border-bottom-left-radius: 5px;
-            width: 40px;
-            text-align: center;
-          }
-        }
-        input {
-          padding: 10px;
-          font-size: 15px;
-          font-family: 'Montserrat', sans-serif;
-          border: 0;
-          border-top-right-radius: 5px;
-          border-bottom-right-radius: 5px;
-          border-left: 1px solid #f9f9f9;
-          outline: none;
-        }
-        .validation-error {
-          display: flex;
-          position: absolute;
-          left: calc(100% + 10px);
-          top: 0;
-          height: 100%;
-          align-items: center;
-          span {
-            background: rgba(163, 0, 0, 0.75);
-            color: #fff;
-            border-radius: 5px;
-            padding: 5px;
-            animation: fadein 0.5s;
-            white-space: nowrap;
-          }
-
-          @keyframes fadein {
-            from {
-              opacity: 0;
-            }
-            to {
-              opacity: 1;
-            }
-          }
-        }
-      `}</style>
-    </div>
-  )
-}
+import renderField from './InputField'
 
 const ItemForm = props => {
-  const { handleSubmit, submitting, url, title, tags } = props
+  const { handleSubmit, submitting } = props
   return (
     <div className="page">
       <form onSubmit={handleSubmit}>
@@ -91,28 +12,26 @@ const ItemForm = props => {
           name="url"
           type="text"
           component={renderField}
-          label="URL"
+          placeholder="URL"
           id="url-field"
-          iconName={faCopy}
-          inputValue={url}
+          iconName="copy"
         />
         <Field
+          autoFocus
           name="title"
           type="text"
           component={renderField}
-          label="Title"
+          placeholder="Title"
           id="title-field"
-          iconName={faEdit}
-          inputValue={title}
+          iconName="edit"
         />
         <Field
           name="tags"
           type="text"
           component={renderField}
-          label="Tags (comma-separated)"
+          placeholder="Tags (comma-separated)"
           id="tags-field"
-          iconName={faTags}
-          inputValue={tags}
+          iconName="tags"
         />
         <Button positive loading={submitting} type="submit">
           Collect
