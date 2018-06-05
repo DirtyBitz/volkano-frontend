b4432107c6bc6e6b7ed06698f271102208c844bf
Mellet Solbakk <mellet89@gmail.com>
2018-06-05 12:15:48 +0200

    :pencil: Fix static images

--- frontend/components/Welcome.tsx
+++ frontend/components/Welcome.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
+import Link from 'next/link'
 import Router from 'next/router'
-import { Button, Segment, Grid, Container, Card, Image } from 'semantic-ui-react'
+import { Button, Grid, Container, Card, Image, Icon } from 'semantic-ui-react'
 import Layout from './Layout'
 
 const Welcome = () => {
@@ -25,51 +26,80 @@ const Welcome = () => {
         </div>
       </div>
 
-      <div className="info-boxes">
+      <Container>
+        <Grid stackable centered columns="three">
+          <Grid.Column>
+            <Card>
+              <Image src="/static/images/idea.jpg" />
+              <Card.Content>
+                <Card.Header>Our vision</Card.Header>
+                <Card.Description>
+                  Our goal through this project is to bring you joy when seeing images you
+                  would love to collect online. We have a wish that we can end the
+                  frustration people experience when they see an image they want to
+                  collect, but do not want to download it onto their current device.
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+
+          <Grid.Column>
+            <Card>
+              <Image src="/static/images/product.jpg" />
+              <Card.Content>
+                <Card.Header>Our product</Card.Header>
+                <Card.Description>
+                  Volkano is a place where you can easily save images, gifs,
+                  youtube-videos and more you come across online, in a single place. You
+                  can also browse your collection and easily share your items.
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+
+          <Grid.Column>
+            <Card>
+              <Image src="/static/images/team.jpg" />
+              <Card.Content>
+                <Card.Header>Who are we?</Card.Header>
+                <Card.Description>
+                  We are a group of students from UiT The Arctic University of Norway
+                  working on a group project to create something beautiful.
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        </Grid>
+      </Container>
+
+      <div id="get-started">
         <Container>
-          <Grid stackable centered columns="three">
-            <Grid.Column>
-              <Card>
-                <Image src="https://cdn.pixabay.com/photo/2017/03/07/13/02/thought-2123970_1280.jpg" />
-                <Card.Content>
-                  <Card.Header>Our vision</Card.Header>
-                  <Card.Description>
-                    Our goal through this project is to bring you joy when seeing images
-                    you would love to collect online. We have a wish that we can end the
-                    frustration people experience when they see an image they want to
-                    collect, but do not want to download it onto their current device.
-                  </Card.Description>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
+          <h2>Get started</h2>
+          <div id="get-started-steps">
+            <div className="step">
+              <Icon name="signup" size="huge" />
+              <h3>Sign up</h3>
+              <p>
+                <Link href="/signup">
+                  <a>Create</a>
+                </Link>{' '}
+                a FREE account
+              </p>
+            </div>
 
-            <Grid.Column>
-              <Card>
-                <Image src="https://cdn.pixabay.com/photo/2018/05/08/08/44/artificial-intelligence-3382507_1280.jpg" />
-                <Card.Content>
-                  <Card.Header>Our product</Card.Header>
-                  <Card.Description>
-                    Volkano is a place where you can easily save images, gifs,
-                    youtube-videos and more you come across online, in a single place. You
-                    can also browse your collection and easily share your items.
-                  </Card.Description>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
+            <div className="step">
+              <Icon name="plus" size="huge" />
+              <h3>Add items</h3>
 
-            <Grid.Column>
-              <Card>
-                <Image src="https://cdn.pixabay.com/photo/2018/05/12/11/37/team-3393037_1280.jpg" />
-                <Card.Content>
-                  <Card.Header>Who are we?</Card.Header>
-                  <Card.Description>
-                    We are a group of students from UiT The Arctic University of Norway
-                    working on a group project to create something beautiful.
-                  </Card.Description>
-                </Card.Content>
-              </Card>
-            </Grid.Column>
-          </Grid>
+              <p>Start collection your media</p>
+            </div>
+
+            <div className="step">
+              <Icon name="coffee" size="huge" />
+              <h3>Chill out</h3>
+              <p>Gain the max chill effect.</p>
+            </div>
+          </div>
         </Container>
       </div>
 
@@ -121,8 +151,26 @@ const Welcome = () => {
           }
         }
 
-        .info-boxes {
-          margin-top: 15px;
+        #get-started {
+          margin-left: -30px;
+          margin-right: -30px;
+          margin-top: 30px;
+          background: #fff;
+          padding: 30px;
+          margin-bottom: -30px;
+
+          h2 {
+            text-align: center;
+            margin-bottom: 60px;
+          }
+        }
+
+        #get-started-steps {
+          display: flex;
+          justify-content: space-between;
+          .step {
+            text-align: center;
+          }
         }
       `}</style>
     </Layout>
