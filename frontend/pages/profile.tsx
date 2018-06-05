f22360ddc60427a4b76c2a820b9f03e6ce30edab
IsakSundeSingh <isak.sunde.singh@gmail.com>
2018-06-05 17:20:35 +0200

    :hammer: Simplify profile-page logic

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -1,22 +1,13 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
 import Profile from '../components/Profile'
-import { getSession } from '../utils/Session'
 import { withAuth } from '../utils/withAuth'
-import { connect } from 'react-redux'
 import { compose } from 'redux'
 
-class ProfilePage extends React.Component<{ req; isSignedIn }, {}> {
-  render() {
-    const session = getSession()
-    return (
-      <Layout title="Profile">
-        <Profile session={session} />
-      </Layout>
-    )
-  }
-}
+const ProfilePage = () => (
+  <Layout title="Profile">
+    <Profile />
+  </Layout>
+)
 
-const composedProfilePage = compose(withAuth)(ProfilePage)
-
-export default connect()(composedProfilePage)
+export default compose(withAuth)(ProfilePage)
